8aa08b6ab559db40ea40949cdb8b4115d4da2acf
AsimMahmudov <mahmudovasim799@gmail.com>
2025-12-01 23:37:50 +0600

    dev

---
+++ src/app/history/delivery-history/page.tsx
@@ -0,0 +1,6 @@
+import DeliveryHistory from '@/components/pages/history/delivery-history/DeliveryHistory';
+import React from 'react';
+
+const DeliveryHistoryPage = () => <DeliveryHistory/>
+
+export default DeliveryHistoryPage;
