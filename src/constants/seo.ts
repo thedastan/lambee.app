df8314ab12ec683241c954e28c68e3bd725d784c
Dastan Mukeev <55836145+thedastan@users.noreply.github.com>
2026-02-02 18:06:01 +0600

    Update site name and SEO keywords in seo.ts

--- src/constants/seo.ts
+++ src/constants/seo.ts
@@ -1,6 +1,7 @@
-export const SITE_NAME = 'Lambee - подгузники премиум-класса'
+export const SITE_NAME = 'Lambee Probiotic - инновационная забота о малыше'
 
 export const SITE_DESCRIPTION = 'Качество премиум-класса для здоровья и удобства с первых минут жизни'  
 
-export const SEO_KEY_WORDS = 'Спокойный малыш,подгузники, Антиаллергенные,мягкие,малыш'
+export const SEO_KEY_WORDS = 'Спокойный малыш, подгузники, Антиаллергенные, мягкие, малыш, трусики, тревел-паки, салфетки, крем'
+
  
