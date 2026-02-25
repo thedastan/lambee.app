5147b9f298812b8fe03237e86d3c529fa9446d62
AsimMahmudov <mahmudovasim799@gmail.com>
2026-02-25 15:13:19 +0600

    Add Google Tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,54 +1,75 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import Script from "next/script"; // Импортируем компонент для скриптов
 import "@/styles/globals.scss";
 import Head from "./Head";
 import { SITE_DESCRIPTION, SITE_NAME } from "@/constants/seo";
 import Providers from "./provider";
 
 const geistSans = localFont({
-	src: "./fonts/GeistVF.woff",
-	variable: "--font-geist-sans",
-	weight: "100 900",
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
 });
 const geistMono = localFont({
-	src: "./fonts/GeistMonoVF.woff",
-	variable: "--font-geist-mono",
-	weight: "100 900",
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
 });
 
 export const metadata: Metadata = {
-	title: {
-		absolute: SITE_NAME,
-		template: `%s | ${SITE_NAME}`,
-	},
-	description: SITE_DESCRIPTION,
-	openGraph: {
-		title: SITE_NAME,
-		description: SITE_DESCRIPTION,
-		siteName: SITE_NAME,
-		images: [
-			{
-				url: "/logo.png",
-				alt: SITE_NAME,
-			},
-		],
-		locale: "ru_RU",
-		type: "website",
-	},
+  title: {
+    absolute: SITE_NAME,
+    template: `%s | ${SITE_NAME}`,
+  },
+  description: SITE_DESCRIPTION,
+  openGraph: {
+    title: SITE_NAME,
+    description: SITE_DESCRIPTION,
+    siteName: SITE_NAME,
+    images: [
+      {
+        url: "/logo.png",
+        alt: SITE_NAME,
+      },
+    ],
+    locale: "ru_RU",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<Head/>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-				<Providers>{children}</Providers>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <Head />
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager (Script) */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KSFVVJM6');
+          `}
+        </Script>
+
+        {/* Google Tag Manager (noscript) - вставляется сразу после <body> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KSFVVJM6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
 }
